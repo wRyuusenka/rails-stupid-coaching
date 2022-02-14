1355726af6f7b20a88a7977874370a38bad12993
Winston Chu <r.winstonc@gmail.com>
2022-02-14 15:04:38 +0900

    Stupid Coach done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,17 @@
 class QuestionsController < ApplicationController
-    def home
+    def ask
+        # @ask = params[:question]
     end
+
+    def answer
+			@question = params[:question]  #param is called through "name"
+        if @question == "I am going to work" 
+					@answer = "Great!"
+				elsif @question.ends_with?("?")
+					@answer = "Silly question, get dressed and go to work!"
+				else 
+					@answer = "I don't care, get dressed and go to work!"
+				end
+    end
+
 end
